007089f7e2e923856638ce8a60a172680e8aba8f
goasguenl <loic.goasguen@mail-alpha.com>
2017-03-28 15:03:46 +0200

    MongoDB

--- pachimari/src/main/java/com/pachimari/MongoConfig.java
+++ pachimari/src/main/java/com/pachimari/MongoConfig.java
@@ -1,29 +1,26 @@
 package com.pachimari;
 
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-/**
- * Created by Pierre on 25/03/2017.
- */
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
 @Configuration
-@EnableMongoRepositories(basePackages = "com.pachimari.user.repository")
-public class MongoConfig {
+public class MongoConfig extends  AbstractMongoConfiguration {
 
-    @Bean
-    public Mongo mongo() throws Exception {
-        return new MongoClient("localhost");
+
+    @Override
+    public String getDatabaseName() {
+        return "pachimari";
     }
 
+    @Override
     @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "test");
+    public Mongo mongo() throws Exception {
+        return new MongoClient("127.0.0.1");
     }
 
 }
